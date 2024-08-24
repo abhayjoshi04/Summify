f8e588ea20c0b48fb7ebdb329d66a4b4959f5a1e
Abhay Joshi <abhayjoshi.avj@gmail.com>
2024-08-24 13:10:25 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -8,15 +8,15 @@ textArea.addEventListener("input", verifyTextLength);
 submitButton.addEventListener("click", submitData);
 
 function verifyTextLength(e) {
- // The e.target property gives us the HTML element that triggered the event, which in this case is the textarea. We save this to a variable called 'textarea'
+ 
   const textarea = e.target;
 
-  // Verify the TextArea value.
+
   if (textarea.value.length > 200 && textarea.value.length < 100000) {
-    // Enable the button when text area has value.
+    
     submitButton.disabled = false;
   } else {
-    // Disable the button when text area is empty.
+ 
     submitButton.disabled = true;
   }
 }
@@ -42,15 +42,13 @@ function submitData(e) {
     redirect: 'follow'
   };
 
-  // Send the text to the server using fetch API
+ 
 
- // Note - here we can omit the “baseUrl” we needed in Postman and just use a relative path to “/summarize” because we will be calling the API from our Replit!  
+  
   fetch('/summarize', requestOptions)
-    .then(response => response.text()) // Response will be summarized text
+    .then(response => response.text()) 
     .then(summary => {
-      // Do something with the summary response from the back end API!
-
-      // Update the output text area with new summary
+      
       summarizedTextArea.value = summary;
 
       // Stop the spinning loading animation
